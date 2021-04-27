261e4c39e00c219a4972cdef9f603815773c6271
LucasMW <great-lucas@hotmail.com>
2021-04-27 23:13:44 +0100

    my modifications

---
+++ Assets/DestructibleBox.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleBox : MonoBehaviour
+{
+    public GameObject itself;
+    public float HP = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Flash() {
+      Renderer renderer = GetComponentInChildren<Renderer>();
+      Material mat = renderer.material;
+     
+      float emission = Mathf.PingPong(Time.time, 1.0f);
+      Color baseColor = Color.red; //Replace this with whatever you want for your base color at emission level '1'
+     
+      Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+ 
+      mat.SetColor("_EmissionColor", finalColor);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(HP <= 0){
+            Debug.Log("Should Destroy");
+            Destroy(itself);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log(collision);
+    }
+    void OnParticleCollision(GameObject other)
+    {
+
+        //Debug.Log(other.name);
+        if(other.name == "NormalBeamParticle") {
+            HP -= 10.0f;
+        } else if(other.name == "ChargedBeamParticle"){
+            HP -= 70.0f;
+        }
+        Flash();
+        //Perhaps play flashing animation?
+        Debug.Log(HP);
+       
+    }
+
+}
---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+	public GameObject itself;
+    public GameObject lightRef;
+	public GameObject playerRef;
+    public float awereness;
+	private bool seesYou; 
+    public ParticleSystem cannonParticleShooter;
+
+    private float TimeInterval ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seesYou = false;
+        lightRef.GetComponent<Renderer> ().material.color = Color.green;
+        //awereness = 30;
+        //cannonParticleShooter = itself.GetComponentInChildren<ParticleSystem>();
+    }
+
+    void Shoot() {
+        Debug.Log("Shoot");
+        cannonParticleShooter.Play();
+    }
+
+    void LateUpdate()
+    {
+        // ones per in seconds
+        if(seesYou){
+            TimeInterval += Time.deltaTime;
+
+            if (TimeInterval >= 0.4)
+            {
+                Shoot();
+            }
+            if (TimeInterval >= 1)
+            {
+                TimeInterval = 0;
+            
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(itself.transform.position, playerRef.transform.position);
+        if(dist < awereness) {
+            seesYou = true;
+            lightRef.GetComponent<Renderer> ().material.color = Color.red;
+
+        } else {
+            seesYou = false;
+            lightRef.GetComponent<Renderer> ().material.color = Color.green;
+        }
+
+        if(seesYou) {
+            var mem = itself.transform.rotation;
+            Vector3 target = new Vector3(playerRef.transform.position.x,itself.transform.position.y, playerRef.transform.position.z);
+            itself.transform.LookAt(target);
+            //Shoot();
+        }
+        
+        
+    }
+}
