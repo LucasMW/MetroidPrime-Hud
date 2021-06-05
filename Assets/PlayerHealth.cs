7782170bd9e468cad73abfd3e5ec9063800ba1a3
LucasMW <great-lucas@hotmail.com>
2021-06-05 11:12:56 +0100

    lifebar and ETanks

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -10,12 +11,37 @@ public class PlayerHealth : MonoBehaviour
     public AudioClip damageAudio;
     public AudioSource audioSource;
     public GameObject visor;
+    public GameObject lifeNumberText;
+    public GameObject lifeBar;
+    public GameObject energyTankGroup;
     // Start is called before the first frame update
     void Start()
     {
         life = 100;
         visor = GameObject.Find("vidro");
         Debug.Log(visor);
+    }
+    void setEnergyTanks(int amount) {
+        Transform[] allChildren  = energyTankGroup.GetComponentsInChildren<Transform>();
+
+        Debug.Log("allChildren " + allChildren.Length);
+        foreach (Transform child in allChildren)
+        {
+            Image img = child.gameObject.GetComponent<Image>();
+            Debug.Log(child.gameObject);
+            Debug.Log(img);
+            if(img != null){
+                img.enabled = false;
+            }
+            //img.enabled = false;
+            //child.gameObject.SetActive(false);
+        }
+        for(int i = 0; i < amount+1; i++){
+           Image img = allChildren[i].gameObject.GetComponent<Image>();
+           if(img != null){
+                img.enabled = true;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -26,6 +52,13 @@ public class PlayerHealth : MonoBehaviour
         	GameOver();
         	//Destroy(itself);
         }
+        int lifeNumber = ((int)life) % 100;
+        lifeNumberText.GetComponent<Text>().text =  lifeNumber < 10 ? "0"+ lifeNumber : lifeNumber + "";
+        Debug.Log(lifeNumber);
+        int energyTankSquares = ((int)life) / 100;
+        Debug.Log(energyTankSquares);
+        setEnergyTanks(energyTankSquares);
+        lifeBar.GetComponent<RectTransform>().localScale = new Vector3(1.0F*lifeNumber/99.0F, 1f, 1f);
     }
 
     void Flash() {
